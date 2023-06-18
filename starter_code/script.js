5dcaee7a05ad1805252db523c2e56c10e51789c1
dianaloz <dlbogota2104@gmail.com>
2023-06-18 15:35:54 -0400

    Added files

---
+++ starter_code/script.js
@@ -0,0 +1,123 @@
+const input = document.querySelector("#fruit");
+const suggestions = document.querySelector(".suggestions ul");
+
+const fruit = [
+  "Apple",
+  "Apricot",
+  "Avocado 🥑",
+  "Banana",
+  "Bilberry",
+  "Blackberry",
+  "Blackcurrant",
+  "Blueberry",
+  "Boysenberry",
+  "Currant",
+  "Cherry",
+  "Coconut",
+  "Cranberry",
+  "Cucumber",
+  "Custard apple",
+  "Damson",
+  "Date",
+  "Dragonfruit",
+  "Durian",
+  "Elderberry",
+  "Feijoa",
+  "Fig",
+  "Gooseberry",
+  "Grape",
+  "Raisin",
+  "Grapefruit",
+  "Guava",
+  "Honeyberry",
+  "Huckleberry",
+  "Jabuticaba",
+  "Jackfruit",
+  "Jambul",
+  "Juniper berry",
+  "Kiwifruit",
+  "Kumquat",
+  "Lemon",
+  "Lime",
+  "Loquat",
+  "Longan",
+  "Lychee",
+  "Mango",
+  "Mangosteen",
+  "Marionberry",
+  "Melon",
+  "Cantaloupe",
+  "Honeydew",
+  "Watermelon",
+  "Miracle fruit",
+  "Mulberry",
+  "Nectarine",
+  "Nance",
+  "Olive",
+  "Orange",
+  "Clementine",
+  "Mandarine",
+  "Tangerine",
+  "Papaya",
+  "Passionfruit",
+  "Peach",
+  "Pear",
+  "Persimmon",
+  "Plantain",
+  "Plum",
+  "Pineapple",
+  "Pomegranate",
+  "Pomelo",
+  "Quince",
+  "Raspberry",
+  "Salmonberry",
+  "Rambutan",
+  "Redcurrant",
+  "Salak",
+  "Satsuma",
+  "Soursop",
+  "Star fruit",
+  "Strawberry",
+  "Tamarillo",
+  "Tamarind",
+  "Yuzu",
+];
+
+function search(str) {
+  let results = [];
+  let inputVal = str;
+  results = fruit.filter((value) => {
+    return value.toLowerCase().includes(str.toLowerCase());
+  });
+  showSuggestions(results, inputVal);
+}
+
+function searchHandler(e) {
+  let str = e.target.value;
+  search(str);
+}
+
+function showSuggestions(results, inputVal) {
+  const liElements = document.querySelectorAll("li");
+  for (let element of liElements) {
+    element.remove();
+  }
+  console.log(results);
+  for (let result of results) {
+    let liElement = document.createElement("li");
+    liElement.textContent = result;
+    suggestions.appendChild(liElement);
+  }
+}
+
+function useSuggestion(e) {
+  input.value = "";
+  input.value = e.target.innerText;
+  const liElements = document.querySelectorAll("li");
+  for (let element of liElements) {
+    element.remove();
+  }
+}
+
+input.addEventListener("keyup", searchHandler);
+suggestions.addEventListener("click", useSuggestion);
